e449f8460f01cc15a653050a0549d44f416e839d
Arthur Vinicius <150489356+ArthurS-Almeida@users.noreply.github.com>
2026-06-14 19:39:47 -0300

    Desenvolvimento de componente e implemento na pagina home.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-//import Profile from './pages/Profile';
-//import Settings from './pages/Settings';
-
-// Criando componentes 
-const home = Home;
-const Diego = () => <div>Página de perfil do Diego</div>;
-const Rodrigo = () => <div>Página de configurações do Rodrigo</div>;
+import './styles/root.css';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<home />} />
+        <Route path="/" element={<Home />} />
+        {/*
         <Route path="/diego" element={<Diego />} />
         <Route path="/rodrigo" element={<Rodrigo />} />
+        */}
       </Routes>
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,72 @@
-function Home(){
-    return(
+import Card from '../components/card.js';
+
+
+
+function Home() {
+    return (
         <div>
-            
+            <header>
+                <figure>
+                    <span>Sua Logo</span>
+                </figure>
+                <nav>
+                    <ul>
+                        <li>Sobre</li>
+                        <li>Contato</li>
+                        <li>Unidade</li>
+                        <li><button>Login</button></li>
+
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <div>
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="..." class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="..." class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="..." class="d-block w-100" alt="..." />
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </main>
+            <section> {/* Patrocinio */}
+                <div>
+                    <marquee>AAAAAA</marquee>
+                </div>
+            </section>
+            <section>
+                <article>
+                    <h1>Blog:</h1>
+                    <aside>
+                        <Card />
+                    </aside>
+                </article>
+            </section>
+            <footer>
+                <section>Empresa</section>
+                <section>Contato</section>
+                <section>Redes Sociais</section>
+            </footer>
         </div>
     )
 }
